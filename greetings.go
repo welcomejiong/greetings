0fd0a9c17ef585c0e5b056bee4a3ca8878b9b275
郭建炯 <guojianjiong@hoolai.com>
2021-02-20 09:47:00 +0800

    举例说明

--- greetings.go
+++ greetings.go
@@ -9,7 +9,7 @@ import (
 func Hello(name string) (string, error) {
 	// Return a greeting that embeds the name in a message.
 	if name == "" {
-		return "", errors.New("empty name please enter your name")
+		return "", errors.New("empty name please enter your name.eg:lingxiaoxiao")
 	}
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 	return message, nil
